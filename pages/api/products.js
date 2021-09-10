e6f602386cccaf10af0a496519f9bbad641e8c4d
serii <seriiburduja@gmail.com>
2021-09-10 16:59:44 +0300

    single product

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,7 +2,8 @@ import products from "./../../data/products";
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    res.status(200).json({ data: products })
+    res.status(200).json({ status: "success", data: products, count: products.length })
+  } else {
+    res.status(200).json({ name: 'John Doe' })
   }
-  res.status(200).json({ name: 'John Doe' })
 }
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,29 +1,57 @@
 import React from 'react';
 import axios from "axios";
+import styles from "./SingleProduct.module.scss";
+import Image from "next/image";
+import Rating from "../../components/Rating";
+import { Button } from "antd";
 
-const SingleProduct = () => {
+const SingleProduct = ({ product }) => {
+  console.log(product, 'product')
+  const { name, image, rating, numReviews, price, description, countInStock } = product;
   return (
-    <div>
-
+    <div className={'container'}>
+      <div className={styles.product}>
+        <div className={styles.image}>
+          <Image src={image} alt={name} layout={'responsive'} width={800} height={600}/>
+        </div>
+        <div className={styles.content}>
+          <h1>{name}</h1>
+          <div className="bb"/>
+          <Rating rating={rating} text={numReviews}/>
+          <div className="bb"/>
+          <div className={styles.price}>Price: <strong>${price}</strong></div>
+          <div className="bb"/>
+          <div className={styles.description}>{description}</div>
+          <div className="bb"/>
+          <div className={styles.stock}>
+            Status: {countInStock >= 1 ? 'In stock' : 'Out of stock'}
+          </div>
+          <div className={styles.footer}>
+            <Button disabled={countInStock === 0} type={'primary'}>Add to cart</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export async function getServerSideProps(context) {
-  try {
-    const products = await axios(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`);
-    // console.log(products.data, 'products');
-
-  } catch (e) {
-    console.log(e, 'e')
-  }
   const id = context.query.id;
-
-  return {
-    props: {
-      post: []
+  try {
+    const { data } = await axios(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`);
+    const product = data.data.find(item => item._id === id);
+    return {
+      props: {
+        product: product
+      }
     }
-  };
+  } catch (e) {
+    return {
+      props: {
+        product: {}
+      }
+    }
+  }
 }
 
 export default SingleProduct;
